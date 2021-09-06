5272419b4e03c535fda5c93ee516a6cd0e6d560a
Mayuresan08 <87636548+Mayuresan08@users.noreply.github.com>
2021-09-06 17:25:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+//  import List from './listKey';
+import Lifecycle from './lifecycle1'
+
+class Main extends React.Component
+{
+  constructor(props)
+  {
+    super(props)
+    this.state={
+      id:0,
+    show:true}
+  }
+  render()
+  {
+    return(
+      <>
+      <h1>Parent Component</h1>
+      <h2>ID:{this.state.id}</h2>
+      <button onClick={()=>{this.setState({id:this.state.id+1})}}>Change ID</button>
+      <button onClick={()=>{this.setState({show:(!this.state.show)})}}>Show/Hide child Component</button>
+      {this.state.show? <Lifecycle id={this.state.id}/>:<></>}
+      {/*</Lifecycle> */}
+      </>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+    <Main/>,
+  document.getElementById('root')
+);
+
+
+
---
+++ lifecycle1.js
@@ -0,0 +1,50 @@
+import React from "react";  
+
+class Lifecycle extends React.Component
+{
+    constructor(props)
+    {console.log("in Constructor",props.id)
+        super(props)
+        this.state={counter:0}
+        
+    }
+    componentDidMount()
+    {
+        console.log("in ComponentDidMount")
+    }
+    handleClick=(val)=>{
+        console.log(val)
+       if(val==="+") this.setState((prevState)=>{return{counter:prevState.counter+1}})
+       else if(val==="-") this.setState((prevState)=>{return{counter:prevState.counter-1}})
+       else this.setState({counter:0})
+    }
+    shouldComponentUpdate()
+    { console.log("in shouldComponentUpdate")
+        return true
+    }
+    componentDidUpdate(){
+        console.log("in ComponentDidUpdate")
+    }
+    componentWillUnmount()
+    {
+        console.log("in componentWillUnmount")
+    }
+    render()
+    {
+        console.log("in render",this.props.id)
+        return(
+            <>
+            <h1>Child component</h1>
+            <h2>Counter :{this.state.counter}</h2>
+            {/* passing arguments via arrow function */}
+            <button onClick={()=>this.handleClick("+")}>Increment</button>
+            {/* passing arguments via bind() */}
+            <button onClick={this.handleClick.bind(this,"-")}>Decrement</button>
+            <button onClick={()=>this.handleClick("")}>Reset</button>
+            <button onClick={()=>this.forceUpdate()}>forceUpdate()</button>
+            </>
+        )
+    }
+}
+
+export default Lifecycle
